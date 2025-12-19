77a5cbae5c9244c0623fb918cdc154d7c4f50f2f
Param Singh <param@example.com>
2025-12-20 02:13:43 +0530

    fixing errors

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     outDir: "dist",
-    chunkSizeWarningLimit: 1600,
+    chunkSizeWarningLimit: 3000,
     rollupOptions: {
       external: ['buffer'],
     },
